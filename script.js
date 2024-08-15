5e4076dbb72275aa95fb3379011436518b782283
Patrick Gross <bears_lair@protonmail.com>
2024-08-15 16:26:17 -0700

    palindrome checker script complete

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
-//Palindrome Checker Algorithm:
-//string to lower-case
-//filter out all non-alpha characters via regular expression
-//change to array of characters
-//reverse array with forEach and unshift array methods
-//join reversed array into reversed string
-//compare original string to reversed string
-//return if there is a match or not with ternary operator
-
 const inputField = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
+const resultText = document.getElementById("result");
 
 const palindromeChecker = (inputText) => {
-  console.log("inputText is... ", inputText);
-
-  const pureText = inputText.toLowerCase().replace(/[^a-z]/g, "");
-  console.log("pureText is... ", pureText);
+  const pureText = inputText.toLowerCase().replace(/[^a-z0-9]/g, "");
 
   let reversedArr = [];
 
   pureText.split("").forEach((char) => reversedArr.unshift(char));
-  console.log("reversedArr is now... ", reversedArr);
 
   const reversedStr = reversedArr.join("");
-  console.log("reversedStr is... ", reversedStr);
 
   return pureText === reversedStr
     ? `${inputText} is a palindrome`
@@ -35,7 +22,8 @@ checkBtn.addEventListener("click", () => {
   } else {
     let inputText = inputField.value;
     let result = palindromeChecker(inputText);
-    console.log("result text is...", result);
+
+    resultText.textContent = result;
     inputField.value = "";
   }
 });
